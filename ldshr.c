c1b07682471fcaba1b8da5eddcc80bae37033b5d
Nuo Zhou <nuzhou@chopin.eecs.csuohio.edu>
2018-12-02 14:36:10 -0500

    SUCCESS: sumqroot_lst

--- ldshr.c
+++ ldshr.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <ctype.h>
 #include <rpc/rpc.h>
 #include "ldshr.h"
@@ -27,11 +28,26 @@ struct gpu_passing{
   double *dp;
 };
 
+struct lst_passing{
+  CLIENT *cl;
+  struct node *p;
+  double dp;
+};
+
+struct file_data{
+	double num;
+	char c;
+};
+
 void check_input(int argc, char *argv[]);
 void check_load();
 void *getload(void *p);
 void *gpu(struct gpu_passing *p);
+void *lst(void *p);
 void run_gpu(char *arg[], int idx1, int idx2);
+void run_lst(char *arg[], int idx1, int idx2);
+void read_file(char *filename, struct node **l1, struct node **l2);
+// void write_file();
 
 
 main(argc, argv)
@@ -46,7 +62,172 @@ main(argc, argv)
   // Run gpt
   if(strcmp(argv[1], "-gpu")==0){
     run_gpu(argv, min_idx[0], min_idx[1]);
+  }
+  if(strcmp(argv[1], "-lst")==0){
+    run_lst(argv, min_idx[0], min_idx[1]);
   }  
+}
+
+void run_lst(char *argv[], int idx1, int idx2){
+	// Variables
+	struct node *list1;
+	struct node *list2, *tmp;
+	struct lst_passing p1, p2;
+	pthread_t p_id1, p_id2;
+	// Contents
+	read_file(argv[2], &list1, &list2);
+	if (list1==NULL && list2==NULL)
+	{
+		printf("No data in %s.\n", argv[2]);
+	}
+	else if (list1!=NULL && list2==NULL)
+	{
+		tmp = list1;
+		printf("\n%s linked list is ", srvname[idx1]);
+		while(tmp){
+			printf("%.3f\t", tmp->num);
+			tmp = tmp->next;
+		}
+		printf("\n");
+		p1.cl = cl[idx1];
+		p1.p = list1;
+		p1.dp = -1;
+		if(pthread_create(&p_id1, NULL, lst, (void *)&p1) !=0){
+	      printf("Create new thread failed! Program terminates!\n");
+	      exit(0);
+	  	}
+	  	pthread_join(p_id1, NULL); 
+	  	printf("%s returns %.3f. Sum is %.3f\n", srvname[idx1], p1.dp, p1.dp);
+	}
+	else if (list1==NULL && list2!=NULL)
+	{
+		tmp = list2;
+		printf("\n%s linked list is ", srvname[idx1]);
+		while(tmp){
+			printf("%.3f\t", tmp->num);
+			tmp = tmp->next;
+		}
+		printf("\n");
+		p2.cl = cl[idx2];
+		p2.p = list2;
+		p2.dp = -1;
+		if(pthread_create(&p_id2, NULL, lst, (void *)&p2) !=0){
+	      printf("Create new thread failed! Program terminates!\n");
+	      exit(0);
+	  	}
+	  	pthread_join(p_id2, NULL); 	
+	  	printf("%s returns %.3f. Sum is %.3f\n", srvname[idx2], p2.dp, p2.dp);	
+	}
+	else if (list1!=NULL && list2!=NULL)
+	{
+		tmp = list1;
+		printf("\n%s linked list is ", srvname[idx1]);
+		while(tmp){
+			printf("%.3f\t", tmp->num);
+			tmp = tmp->next;
+		}
+		printf("\n");
+		tmp = list2;
+		printf("\n%s linked list is ", srvname[idx2]);
+		while(tmp){
+			printf("%.3f\t", tmp->num);
+			tmp = tmp->next;
+		}
+		printf("\n");
+		p1.cl = cl[idx1];
+		p1.p = list1;
+		p1.dp = -1;
+		p2.cl = cl[idx2];
+		p2.p = list2;
+		p2.dp = -1;
+		if(pthread_create(&p_id1, NULL, lst, (void *)&p1) !=0){
+	      printf("Create new thread failed! Program terminates!\n");
+	      exit(0);
+	  	}	  	
+		if(pthread_create(&p_id2, NULL, lst, (void *)&p2) !=0){
+	      printf("Create new thread failed! Program terminates!\n");
+	      exit(0);
+	  	}
+	  	pthread_join(p_id1, NULL); 
+	  	pthread_join(p_id2, NULL);
+	  	printf("%s returns %.3f. Sum is %.3f\n", srvname[idx1], p1.dp, p1.dp);
+	  	printf("%s returns %.3f. Sum is %.3f\n", srvname[idx2], p2.dp, p2.dp); 	
+	  	printf("Sum is %.3f\n", p1.dp + p2.dp);
+	}
+}
+
+
+void *lst(void *p){ 
+	struct lst_passing *local = (struct lst_passing *)p;
+	local->dp = *(sumqroot_lst_1(local->p, local->cl));
+}
+
+
+void read_file(char *filename, struct node **l1, struct node **l2){
+	struct node *l1_h = NULL;
+	struct node *l1_e = NULL;
+	struct node *l2_h = NULL;
+	struct node *l2_e = NULL;
+	struct node *tmp;
+	struct file_data cell;
+	int flag = 1;
+	FILE *fptr;
+	if ((fptr = fopen(filename,"r")) == NULL){
+       printf("Error! Read file");
+       exit(0);
+   	}
+   	while(fread(&cell, sizeof(struct file_data), 1, fptr) == 1){
+   		tmp = (struct file_data *)malloc(sizeof(struct file_data));
+   		tmp->num = cell.num;
+   		tmp->next = NULL;
+   		if (flag == 1)
+   		{
+   			flag = 2;
+   			if (l1_h == NULL)
+   			{
+   				l1_h = tmp;
+   				l1_e = tmp;
+   			}
+   			else{
+   				l1_e->next = tmp;
+   				l1_e = l1_e->next;
+   			}
+   		}
+   		else{
+   			flag = 1;
+   			if (l2_h == NULL)
+   			{
+   				l2_h = tmp;
+   				l2_e = tmp;
+   			}
+   			else{
+   				l2_e->next = tmp;
+   				l2_e = l2_e->next;
+   			}
+   		}
+   	}
+   fclose(fptr); 
+   *l1 = l1_h;
+   *l2 = l2_h;
+}
+
+void check_input(int argc, char *argv[]){
+  if(argc!=3 && argc!=6){
+    printf("Wrong number for parameters!\n");
+    exit(0);
+  }
+  if(strcmp(argv[1], "-gpu") != 0 && strcmp(argv[1], "-lst") != 0 ){
+    printf("Wrong command!\n");
+    exit(0);
+  }
+  if(strcmp(argv[1], "-gpu")==0){
+    if(argc!=6)
+      printf("Wrong parameter input\n");
+  }
+  if(strcmp(argv[1], "-lst")==0){
+    if(argc!=3)
+      printf("Wrong parameter input\n");
+  }
 }
 
 
@@ -161,43 +342,22 @@ void *gpu(struct gpu_passing *p){
 }
 
 
-void check_input(int argc, char *argv[]){
-  if(argc!=3 && argc!=6){
-    printf("Wrong number for parameters!\n");
-    exit(0);
-  }
-  if(strcmp(argv[1], "-gpu") != 0 && strcmp(argv[1], "-lst") != 0 ){
-    printf("Wrong command!\n");
-    exit(0);
-  }
-  if(strcmp(argv[1], "-gpu")==0){
-    if(argc!=6)
-      printf("Wrong parameter input\n");
-  }
-  if(strcmp(argv[1], "-lst")==0){
-    if(argc!=3)
-      printf("Wrong parameter input\n");
-  }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// void write_file(){
+// 	struct file_data cell;
+// 	time_t t;
+// 	int i;
+// 	srand((unsigned) time(&t));
+// 	cell.c = ' ';
+// 	FILE *fptr;
+// 	if ((fptr = fopen("datafile","w")) == NULL){
+//        printf("Error! Write file");
+//        exit(0);
+//    }
+//    for(i=0; i<5; i++)
+//    {
+//       cell.num = double(rand() % 50);
+//       printf("%f\t", cell.num);
+//       fwrite(&cell, sizeof(struct file_data), 1, fptr); 
+//    }
+//    fclose(fptr); 
+// }
--- ldshr_svc_proc.c
+++ ldshr_svc_proc.c
@@ -2,10 +2,16 @@
 #include <string.h>
 #include <rpc/rpc.h>
 #include "ldshr.h"
+#include <math.h>
 
 static double *ldp = NULL;
 
 double reduction(int x, int y, int z);
+double sqroot(double num);
+double local_sum(double num1, double num2);
+void map(double (*f)(double), struct node *list);
+double reduce(double (*f)(double, double), struct node *list);
+
 
 double *
 getload_1_svc(char **srvname, struct svc_req *rqp){
@@ -23,9 +29,71 @@ getload_1_svc(char **srvname, struct svc_req *rqp){
   }
 }
 
+
 double *
 sumqroot_gpu_1_svc(struct gpu_struct *param, struct svc_req *rqp){
 	double  *result = malloc(sizeof(double *));
     *result =reduction(param->N, param->mean, param->seed);
   	return result;
+}
+
+
+double *
+sumqroot_lst_1_svc(struct node *param, struct svc_req *rqp){
+  double  *result = (double *)malloc(sizeof(double));
+  struct node *local = param;
+  struct node *tmp = param;
+  // printf("\n received data: ");
+  // while(tmp){
+  //   printf("%.3f\t", tmp->num);
+  //   tmp = tmp->next;
+  // }
+
+  // printf("\nsqrt data: ");
+  map(sqroot, local);
+  // tmp = local;
+  // while(tmp){
+  //   printf("%.3f\t", tmp->num);
+  //   tmp = tmp->next;
+  // }
+
+  printf("\nstart reduce ......\n");
+  *result = reduce(local_sum, local);
+  printf("finish reduce ......\n");
+  printf("sum of data: %.3f\n",   *result);
+  return result;
+}
+
+
+double sqroot(double num){
+  return sqrt(sqrt(num));
+}
+
+
+double local_sum(double num1, double num2){
+  return num1 + num2;
+}
+
+
+void map(double (*f)(double), struct node *list){
+  struct node *tmp = list;
+  while(tmp){
+    tmp->num = (*f)(tmp->num);
+    tmp = tmp->next;
+  }
+}
+
+
+double reduce(double (*f)(double, double), struct node *list){
+  printf("\tenter reduce\n");
+  struct node *tmp = list;
+  double val = 0;
+  while(tmp){
+    // printf("\ntmp->num = %.3f, val = %.3f\n", tmp->num, val);
+    val = (*f)(val, tmp->num);
+    // printf("val_sum = %.3f\n", val);
+    tmp = tmp->next;
+  }
+  printf("\treturned val is %.3f\n", val);
+  return val;
 }
